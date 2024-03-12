0e594f098e6bf5114e12bc6606e40035836ecf39
NguyenHuy2003 <2121050061@student.humg.edu.vn>
2024-03-12 22:03:25 +0700

    update

--- lib/main.dart
+++ lib/main.dart
@@ -10,10 +10,12 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: HomeScreen(),
-    );
+    return MaterialApp(
+        debugShowCheckedModeBanner: false,
+        home: const HomeScreen(),
+        routes: <String, WidgetBuilder>{
+          '/home': (BuildContext context) => const HomeScreen(),
+        });
   }
 }
 
